0b9b9d48c72d6c57d73b530d6dc5f9659ee26ca3
Ashish Kumar Bostan <99416619+BoshmanOvO@users.noreply.github.com>
2024-05-20 22:39:49 +0530

    Update BookingServices.java

--- src/main/java/com/learning/lakesidehotel/Services/BookingServices.java
+++ src/main/java/com/learning/lakesidehotel/Services/BookingServices.java
@@ -1,2 +1,16 @@
-package com.learning.lakesidehotel.Services;public class BookingServices {
+package com.learning.lakesidehotel.Services;
+
+
+import com.learning.lakesidehotel.Models.BookedRoom;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingServices {
+
+    public List<BookedRoom> getAllBookingsByRoomId(Integer roomid) {
+        return null;
+    }
 }
